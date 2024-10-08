ab2d0da0b6a909cfeab398bbb6a9a2bf145110f3
Md Shahriyar Alam <mdshahriyaralam552@gmail.com>
2024-10-09 03:53:31 +0600

    update title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<title>Covert Ice Alliance - CIA</title>
+			</Head>
 			<body className="dark">
 				<Main />
 				<NextScript />
